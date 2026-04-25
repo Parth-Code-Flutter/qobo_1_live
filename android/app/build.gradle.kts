ffbb065d310792c0c2c524916198b0284c42c56f
Parth-Code-Flutter <parth.mandavia.tristate@gmail.com>
2026-04-25 19:19:22 +0530

    - change min sdk version to 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.qobo.qobo1live.qobo_one_live"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
