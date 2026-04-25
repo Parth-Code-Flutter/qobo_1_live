0f55db30f689c2752936b44ccd4ab970d69c9a08
Parth-Code-Flutter <parth.mandavia.tristate@gmail.com>
2026-04-25 14:40:36 +0530

    - added login screen icons

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -1,4 +1,14 @@
 const String imgPath = 'assets/images';
 const String iconPath = 'assets/icons';
 
+/// Img
 const String kImgSplashScreen = '$imgPath/splash_screen_img.png';
+
+/// PNG Icons
+const String kIconApp = '$imgPath/app_icon.png';
+
+/// SVG Icons
+const String kIconFB = '$imgPath/fb_icon.svg';
+const String kIconGoogle = '$imgPath/google_icon.svg';
+const String kIconMail = '$imgPath/mail_icon.svg';
+const String kIconPassword = '$imgPath/password_icon.svg';
