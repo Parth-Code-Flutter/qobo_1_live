6f6570d48d82783e67fb05758835c8aff9e97030
Parth-Code-Flutter <parth.mandavia.tristate@gmail.com>
2026-05-06 17:11:31 +0530

    - completed live_room_view.dart UI

--- lib/app/user_flow/live_room/views/live_room_view.dart
+++ lib/app/user_flow/live_room/views/live_room_view.dart
@@ -68,50 +68,44 @@ class LiveRoomView extends GetView<LiveRoomController> {
             image: DecorationImage(image: AssetImage(kImgBG), fit: BoxFit.cover),
           ),
           child: SafeArea(
-            child: CustomScrollView(
-              slivers: [
-                SliverPadding(
+            child: Column(
+              children: [
+                Padding(
                   padding: const EdgeInsets.fromLTRB(14, 8, 14, 0),
-                  sliver: SliverToBoxAdapter(
-                    child: Column(
-                      children: [
-                        AppText(
-                          text: 'Live Rooms',
-                          fontSize: TextStyles.k18FontSize,
-                          color: kColorWhite,
-                          style: TextStyles.kSemiBoldPoppins(
-                            fontSize: TextStyles.k18FontSize,
-                            colors: kColorWhite,
-                          ),
-                        ),
-                        Spacing.v12,
-                        _filterAndCategoryRow(categories),
-                        Spacing.v12,
-                        _topBanner(),
-                        Spacing.v12,
-                      ],
-                    ),
+                  child: Column(
+                    children: [
+                      _topHeader(),
+                      Spacing.v12,
+                      _filterAndCategoryRow(categories),
+                      Spacing.v12,
+                      _topBanner(),
+                      Spacing.v12,
+                    ],
                   ),
                 ),
-                SliverPadding(
-                  padding: const EdgeInsets.fromLTRB(14, 0, 14, 8),
-                  sliver: SliverGrid(
-                    delegate: SliverChildBuilderDelegate((context, index) {
-                      final room = rooms[index];
-                      return CommonLiveRoomWidget(
-                        imageUrl: room['image'] as String,
-                        userNameAge: room['nameAge'] as String,
-                        badgeText: room['badge'] as String,
-                        locationText: room['location'] as String,
-                        pointsText: room['points'] as String,
-                        isFavorite: room['favorite'] as bool,
-                      );
-                    }, childCount: rooms.length),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 12,
-                      crossAxisSpacing: 12,
-                      childAspectRatio: 0.8,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(14, 0, 14, 40),
+                    child: GridView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: rooms.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 12,
+                        crossAxisSpacing: 12,
+                        childAspectRatio: 0.8,
+                      ),
+                      itemBuilder: (context, index) {
+                        final room = rooms[index];
+                        return CommonLiveRoomWidget(
+                          imageUrl: room['image'] as String,
+                          userNameAge: room['nameAge'] as String,
+                          badgeText: room['badge'] as String,
+                          locationText: room['location'] as String,
+                          pointsText: room['points'] as String,
+                          isFavorite: room['favorite'] as bool,
+                        );
+                      },
                     ),
                   ),
                 ),
@@ -143,6 +137,62 @@ class LiveRoomView extends GetView<LiveRoomController> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _topHeader() {
+    return Row(
+      children: [
+        Container(
+          width: 44,
+          height: 44,
+          padding: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            color: kColorWhite,
+            shape: BoxShape.circle,
+            border: Border.all(color: const Color(0xB3FFFFFF), width: 1),
+          ),
+          child: const CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=200',
+            ),
+          ),
+        ),
+        Spacing.h10,
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppText(
+                text: LocaleKeys.liveRoomWelcome.tr,
+                fontSize: TextStyles.k14FontSize,
+                color: kColorWhite,
+              ),
+              SemiBoldText(
+                text: LocaleKeys.liveRoomHostName.tr,
+                fontSize: TextStyles.k14FontSize,
+                color: kColorWhite,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            color: kColorWhite,
+            borderRadius: BorderRadius.circular(22),
+          ),
+          child: Center(
+            child: SvgPicture.asset(
+              kIconSearch,
+              width: 18,
+              height: 18,
+              colorFilter: const ColorFilter.mode(kColorPrimary, BlendMode.srcIn),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
